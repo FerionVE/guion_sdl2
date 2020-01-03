1e1e55230bdeb572376372b2825c9f9e6ea325a7
qwertz19281 <dellen903@gmail.com>
2020-01-03 01:35:42 +0100

    fixed handler

--- src/handler/imp.rs
+++ src/handler/imp.rs
@@ -1,3 +1,5 @@
+use guion::core::ctx::Widgets;
+use guion::core::widget::link::Link;
 use guion::core::lazout::Size;
 use guion::core::env::Env;
 use guion::core::util::bounds::Bounds;
@@ -6,23 +8,23 @@ use super::*;
 
 impl<S,C> GuionHandler<C> for Handler<S,C> where S: GuionHandler<C>, C: Context, C::Handler: AsHandler<Self,C> + 'static {
     #[inline] 
-    fn _render<E: Env>(senf: &mut C, i: &E::WidgetID, r: (&mut ERenderer<E>,&Bounds)) {
-        S::_render::<E>(senf,i,r);
+    fn _render<E>(l: Link<E>, r: (&mut ERenderer<E>,&Bounds)) where E: Env<Context=C>, C: Widgets<E> {
+        S::_render::<E>(l,r);
         unimplemented!()
     }
     #[inline] 
-    fn _event<E: Env>(senf: &mut C, i: &E::WidgetID, e: EEvent<E>) {
-        S::_event::<E>(senf,i,e);
+    fn _event<E>(l: Link<E>, e: (EEvent<E>,&Bounds)) where E: Env<Context=C>, C: Widgets<E> {
+        S::_event::<E>(l,e);
         unimplemented!()
     }
     #[inline] 
-    fn _event_root<E: Env>(senf: &mut C, i: &E::WidgetID, e: EEvent<E>) {
-        Self::_event::<E>(senf,i,e);
+    fn _event_root<E>(l: Link<E>, e: (EEvent<E>,&Bounds)) where E: Env<Context=C>, C: Widgets<E> {
+        S::_event_root::<E>(l,e);
         unimplemented!()
     }
     #[inline] 
-    fn _size<E: Env>(senf: &mut C, i: &E::WidgetID) -> Size {
+    fn _size<E>(l: Link<E>) -> Size where E: Env<Context=C>, C: Widgets<E> {
         unimplemented!();
-        S::_size::<E>(senf,i)
+        S::_size::<E>(l)
     }
 }
